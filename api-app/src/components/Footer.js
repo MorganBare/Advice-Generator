b8e29d9e22292635d9387cf10fa66634e0c1bfd8
MorganBare <morganjbare@gmail.com>
2022-04-23 14:15:10 +0200

    removed target

--- api-app/src/components/Footer.js
+++ api-app/src/components/Footer.js
@@ -3,7 +3,7 @@
 export default function Footer(){
     return (
         <div className="attribution">
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
             Coded by <a href="https://github.com/MorganBare">Morgan J. Bare</a>.
         </div>
     )
